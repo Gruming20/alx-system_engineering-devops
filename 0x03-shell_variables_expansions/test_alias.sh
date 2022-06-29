bdf5aef34331414e5ed82ebe606a8794d37f4dfb
Gruming20 <gruming.898@gmail.com>
2022-06-29 10:44:34 -0700

    0x03

--- 0x03-shell_variables_expansions/test_alias.sh
+++ 0x03-shell_variables_expansions/test_alias.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
-shopt -s expand_aliases
-sosurce ./0-alias
-ls
-\ls -l
+total 12                                                                                                                                
+-rw-r--r-- 1 root root 28 Jun 29 10:35 0-alias                                                                                          
+-rw-r--r-- 1 root root  0 Jun 29 10:23 file1                                                                                            
+-rw-r--r-- 1 root root  0 Jun 29 10:24 file2                                                                                            
+-rw-r--r-- 1 root root 12 Jun 28 13:52 README.md                                                                                        
+-rw-r--r-- 1 root root 64 Jun 29 09:50 test_alias.sh         
